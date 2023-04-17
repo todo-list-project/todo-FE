2a067765707d767e7af768035a830fb13d7c8fd7
rudwnok <rudwnok123@naver.com>
2023-04-18 00:05:00 +0900

    Chore: 페이지

---
+++ src/components/tab/Tab.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Tab = () => {
+  return (
+    <div className='tab'>
+      <div className="tab-item"></div>
+    </div>
+  )
+}
+
+export default Tab
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,9 +1,11 @@
+import Tab from 'components/tab/Tab'
 import React from 'react'
 
 const Main = () => {
   return (
     <div>
       메인페이지
+      <Tab />
     </div>
   )
 }
