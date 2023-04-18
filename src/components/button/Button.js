e1ca138fb3911cdba49d9b4b5682f91a9c5d289a
rudwnok <rudwnok123@naver.com>
2023-04-18 18:12:24 +0900

    Fix: 할일추가 버튼 누를시 모달 안나오는 현상 수정

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,8 +1,12 @@
 import React from "react";
 import './button.scss';
 
-const Button = ({ children, classname }) => {
-  return <button className={classname}>{children}</button>;
+const Button = ({ children, classname, onClick }) => {
+  return (
+    <button className={classname} onClick={onClick}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
