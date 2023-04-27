9bef8237e57282ff443ffd4a5c684808787bff0d
rudwnok <rudwnok123@naver.com>
2023-04-27 18:48:14 +0900

    추가

--- src/pages/Mypage/Mypage.js
+++ src/pages/Mypage/Mypage.js
@@ -1,9 +1,15 @@
 import Button from "components/button/Button";
-import Tab from "components/tab/Tab";
-import "./mypage.scss";
 import Header from "components/header/Header";
+import { useState } from "react";
+import "./mypage.scss";
 
 const Mypage = () => {
+  const [tab, setTab] = useState(null);
+
+  const clickTab = (type) => {
+    setTab(type);
+  };
+
   return (
     <div className="page my-page">
       <Header />
@@ -18,20 +24,35 @@ const Mypage = () => {
               <li>
                 <strong>친구</strong>
                 <ul>
-                  <li>그룹관리</li>
-                  <li>공유하기</li>
+                  <li onClick={() => clickTab("group-manage")}>그룹관리</li>
+                  <li onClick={() => clickTab(null)}>공유된 할일?</li>
                 </ul>
               </li>
               <li>
                 <strong>계정</strong>
                 <ul>
-                  <li>수정</li>
+                  <li onClick={() => clickTab("account-edit")}>수정</li>
                 </ul>
               </li>
             </ul>
             <Button classname="default-button delete-button">계정탈퇴</Button>
           </div>
-          <div className="content-item">왼쪽의 탭마다 다른 선택한 내용</div>
+          <div className="content-item">
+            {tab === "group-manage" && (
+              <>
+                <div>
+                  폴더 목록 <br />
+                  폴더 생성 및 삭제 기능 추가
+                </div>
+                <div>
+                  친구 목록 <br />
+                  폴더 목록에서 폴더 클릭하고 친구 목록 클릭하면 친구를 폴더로
+                  이동
+                </div>
+              </>
+            )}
+            {tab === "account-edit" && <>정보 수정</>}
+          </div>
         </div>
       </div>
     </div>
