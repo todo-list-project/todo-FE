c366869b59872448c1659905a16a69df822fd888
rudwnok <rudwnok123@naver.com>
2023-05-30 00:18:27 +0900

    root_api 변경

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,5 +1,5 @@
 export const ROOT_API =
-  "http://ec2-15-165-136-203.ap-northeast-2.compute.amazonaws.com:8080";
+  "http://ec2-15-164-26-23.ap-northeast-2.compute.amazonaws.com:8080";
 export const API_HEADER = {
   "Content-Type": "application/json",
   "Access-Control-Allow-Origin": "*",
