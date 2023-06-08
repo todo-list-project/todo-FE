2fb61c2f1942643cfe8e92816022ee15fcfe9516
rudwnok <rudwnok123@naver.com>
2023-06-08 20:06:11 +0900

    팔로워

--- src/pages/Mypage/_com/follower/Follower.js
+++ src/pages/Mypage/_com/follower/Follower.js
@@ -1,9 +1,39 @@
-import React from "react";
+import axios from "axios";
+import { ROOT_API, API_HEADER } from "constants/api";
+import React, { useState } from "react";
 
 const Follower = () => {
+  const [text, setText] = useState("");
+
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
+  const apply = () => {
+    console.log("text", text);
+    axios
+      .post(
+        `${ROOT_API}/follows`,
+        {
+          email: text,
+        },
+        {
+          headers: {
+            API_HEADER,
+          },
+        }
+      )
+      .then((res) => {
+        console.log("res", res);
+      });
+  };
+
   return (
     <div className="follower">
       팔로워
+      <div className="input-wrap">
+        <input type="text" value={text} onChange={onChange} />
+        <button onClick={apply}>팔로워 요청</button>
+      </div>
     </div>
   );
 };
